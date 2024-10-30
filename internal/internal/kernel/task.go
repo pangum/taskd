3f7446b231e2754681f523786a7c9804184f0723
storezhang <storezhang@gmail.com>
2024-10-30 21:44:04 +0800

    refactor(核心): 适配task.Tasker

--- internal/internal/kernel/task.go
+++ internal/internal/kernel/task.go
@@ -3,23 +3,36 @@ package kernel
 import (
 	"github.com/goexl/task"
 	"github.com/pangum/taskd/internal/internal/model"
-	"github.com/pangum/taskd/internal/internal/repository"
 )
 
 var _ task.Task = (*Task)(nil)
 
 type Task struct {
-	task       *model.Task
-	repository repository.Schedule
+	task *model.Task
 }
 
-func NewTask(task *model.Task, repository repository.Schedule) *Task {
+func NewTask(task *model.Task) *Task {
 	return &Task{
-		task:       task,
-		repository: repository,
+		task: task,
 	}
 }
 
 func (t *Task) Id() uint64 {
 	return t.task.Id
+}
+
+func (t *Task) Target() uint64 {
+	return t.task.Target
+}
+
+func (t *Task) Type() task.Type {
+	return t.task.Type
+}
+
+func (t *Task) Subtype() task.Type {
+	return t.task.Subtype
+}
+
+func (t *Task) Retries() uint32 {
+	return t.task.Retries
 }
